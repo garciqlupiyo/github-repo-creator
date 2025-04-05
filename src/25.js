71f3dd02b9851934c6f2283fa0c955e2cd429b9a
garciqlupiyo <garciqlupiyo@gmail.com>
2025-04-05 05:26:43 +0000

    Add 25.js

---
+++ src/25.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+// Route to serve static files from 'public'
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
